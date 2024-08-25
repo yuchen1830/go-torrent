8a1cbdcc9ec8bf27e66f704a602707b83227412c
yuchen <106485679+yuchen1830@users.noreply.github.com>
2024-08-24 17:29:05 -0700

    Update parser.go

--- parser.go
+++ parser.go
@@ -11,11 +11,11 @@ func Parse(r io.Reader) (*BObject, error) {
 		br = bufio.NewReader(r)
 	}
 	// recursively decreasing
-	b, err := br.Peek(1)
+	b, err := br.Peek(1) // read the peek without advancing the reader's position
 	if err != nil {
 		return nil, err
 	}
-	var ret BObject
+	var res BObject
 	switch {
 	case b[0] >= '0' && b[0] <= '9':
 		// string
@@ -23,16 +23,56 @@ func Parse(r io.Reader) (*BObject, error) {
 		if err != nil {
 			return nil, err
 		}
-		ret.type_ = BSTR
-		ret.val_ = val
+		res.type_ = BSTR
+		res.val_ = val
 	case b[0] == 'i':
 		// int
 		val, err := DecodeInt(br)
 		if err != nil {
 			return nil, err
 		}
-		ret.type_ = BINT
-		ret.val_ = val
-	
+		res.type_ = BINT
+		res.val_ = val
+	case b[0] == 'l':
+		// list
+		br.ReadByte() // read and consume a single byte `l`, advancing the position
+		var list []*BObject
+		for {
+			if p, _ := br.Peek(1); p[0] == 'e' { // p is a 1-length slice of byte `[]byte`
+				br.ReadByte()
+				break
+			}
+			elem, err := Parse(br) // recursive parsing
+			if err != nil {
+				return nil, err
+			}
+			list = append(list, elem)
+		}
+		res.type_ = BLIST
+		res.val_ = list
+	case b[0] == 'd':
+		// map
+		br.ReadByte()
+		dict := make(map[string]*BObject)
+		for {
+			if p, _ := br.Peek(1); p[0] == 'e' {
+				br.ReadByte()
+				break
+			}
+			key, err := DecodeString(br)
+			if err !=  nil {
+				return nil, err
+			}
+			val, err := Parse(br)
+			if err != nil {
+				return nil, err
+			}
+			dict[key] = val
+		}
+		res.type_ = BDICT
+		res.val_ = dict
+	default:
+		return nil, ErrIvd
 	}
+	return &res, nil
 }
